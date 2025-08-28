72b5aa6165a7677ce7a3578d78ad14082bdc6bbc
Mahi-1899 <mahimavakwadi@gmail.com>
2025-08-28 22:02:37 +0530

    added kafka consumer and rest

---
+++ analytic-service/src/main/java/dev/pm/analyticservice/AnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.pm.analyticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticServiceApplication.class, args);
+    }
+
+}
